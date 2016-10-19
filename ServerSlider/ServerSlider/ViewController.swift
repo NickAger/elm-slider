c0b3a61f7e9fa1ea08d9bc5dae8e070123f5614a
Nick Ager <nick.ager@gmail.com>
2016-10-19 02:30:26 +0100

    Update ViewController.swift

--- ServerSlider/ServerSlider/ViewController.swift
+++ ServerSlider/ServerSlider/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import Cocoa
+//import HTTPServer
+//import WebSocketServer
 
 class ViewController: NSViewController {
 
@@ -15,7 +17,7 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//        startWebSocketServer()
     }
 
     @IBAction func sliderChanged(_ sender: NSSlider) {
@@ -27,7 +29,62 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
+// MARK: - web socket server
+//extension ViewController {
+//    func startWebSocketServer() {
+//        let port = 8080
+//        let log = LogMiddleware()
+//        
+//        let router = BasicRouter { route in
+//            route.get("/hello2") { request in
+//                return Response(body: "Hello, world!")
+//            }
+//        }
+//        
+//        do {
+//            let server = try Server(port: port, middleware: [log], responder: router)
+//            try server.start()
+//        } catch {
+//            print("Error = \(error)")
+//        }
+//    }
+//}
+//
+//
+//// MARK: - more socket server stuff
+//extension ViewController {
+//    func startWebSocketServer() {
+//        let server = WebSocketServer { req, ws in
+//            print("Connected!")
+//            ws.onText { text in
+//                print("text: \(text)")
+//                try ws.send(text)
+//            }
+//            ws.onClose {(code, reason) in
+//                print("\(code): \(reason)")
+//            }
+//        }
+//        
+//        try Server(responder: server).start()
+//    }
+//    
+//    
+//    func startFromRequest () {
+//        try Server { request in
+//            return try request.webSocket { req, ws in
+//                print("connected")
+//                
+//                ws.onBinary { data in
+//                    print("data: \(data)")
+//                    try ws.send(data)
+//                }
+//                ws.onText { text in
+//                    print("data: \(text)")
+//                    try ws.send(text)
+//                }
+//            }
+//            }.start()
+//    }
+//}
